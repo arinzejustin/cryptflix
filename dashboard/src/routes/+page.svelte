07e3fa800013bd3466582d6398673ad59d1f03ba
arinzejustin <arinzejustinng@gmail.com>
2023-02-06 17:01:27 +0100

    Update +page.svelte

--- dashboard/src/routes/+page.svelte
+++ dashboard/src/routes/+page.svelte
@@ -1,1 +1,22 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { goto } from '$app/navigation';
+	import Loader from '$lib/Loader.svelte';
+	import { fly } from 'svelte/transition';
 
+	onMount(() => goto('/app', { noScroll: true, keepFocus: true, replaceState: true }));
+</script>
+
+<div
+	out:fly={{ y: -200, opacity: 0 }}
+	class="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+>
+	<Loader width={'80px'} height={'80px'} />
+	<div class="mt-8 text-center">
+		<p
+			class="uppercase mt-2 text-base md:text-lg lg:text-xl text-slate-700 dark:text-slate-50 font-open font-semibold lg:font-extrabold"
+		>
+			Loading Dashboard .....
+		</p>
+	</div>
+</div>
