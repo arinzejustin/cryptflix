128719777481c1449724a4fb9d447b81fb81d0f9
arinzejustin <arinzejustinng@gmail.com>
2023-02-10 18:27:33 +0100

    Update +page.svelte

--- dashboard/src/routes/app/+page.svelte
+++ dashboard/src/routes/app/+page.svelte
@@ -5,5 +5,5 @@
 <svelte:head />
 
 <div in:fly={{ x: 400 }} out:fly={{ x: -400 }} class="flex container">
-	div>
+	<div class="bg-yellow-100 dark:bg-yellow-100/30 theme-text-app border border-solid border-yellow-200 shadow-lg rounded-md p-4"></div>
 </div>
