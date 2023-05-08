bf86fdf10d8bb20af089bd0ab1bbf139643d3c7c
arinzejustin <arinzejustinng@gmail.com>
2023-05-08 02:11:01 +0100

    update

---
+++ dashboard/src/routes/admin/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { fly } from 'svelte/transition';
+    import type { PageData } from './$types';
+
+	export let data: PageData;
+</script>
+<svelte:head>
+	<title>Dashboard | cryptflixinvest.com</title>
+</svelte:head>
+
+<div in:fly={{ x: 300, delay: 1000 }} out:fly={{ x: -400, duration: 800 }} class="mt-4 pt-5">
+    div>
+</div>
